301e64a14a1e503d2d9b0d3b94e334f38b396a6b
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-05-13 09:34:44 -0600

    created the App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>
+        <a href="/">Open Source Code</a>
+        By Yetunde Ajayi
+      </p>
+    </div>
+  );
 }
 
 export default App;
